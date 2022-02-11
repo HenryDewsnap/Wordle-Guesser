7185a1b282dba746334a8c3bf067b0c1df4afbc8
Henry Dewsnap <74488894+HenryDewsnap@users.noreply.github.com>
2022-02-11 23:06:49 +0000

    Update main.py

--- scr/main.py
+++ scr/main.py
@@ -29,6 +29,7 @@ class wordle_guesser:
         with open("words.csv", "r") as f:
             for word in csv.reader(f): self.in_use_words.append(word)
         
+        self.known_not_positions = [] #the yellow positions
         self.known_positions = [None, None, None, None, None] ##The green ones
         self.uu_letters = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"] ##The light gray ones
         self.bl_letters = [] ##The gray ones
@@ -55,6 +56,11 @@ class wordle_guesser:
                 for word in copy.copy(self.in_use_words[0]):
                     if word[known_pos] != self.known_positions[known_pos] or self.known_positions[known_pos] in f"{word[0:known_pos]}{word[known_pos+1::]}": self.in_use_words[0].remove(word)
 
+        for known_not_pos in self.known_not_positions:
+            print(known_not_pos)
+            for word in copy.copy(self.in_use_words[0]):
+                if word[known_not_pos[1]] == known_not_pos[0]: self.in_use_words[0].remove(word)
+
         for letter in self.bl_letters:
             if letter in self.uu_letters: self.uu_letters.remove(letter)
 
@@ -66,10 +72,17 @@ if __name__ == "__main__":
     wg = wordle_guesser()
     while True:
         wg.check_conditions()
-        for wv in wg.wv: print(f"Word: {wv[0]} Certainty: {wv[1]}")
+        for wv in wg.wv: print(f"Word: {wv[0]} ¦¦ Certainty: {wv[1]}")
         
         wg.bl_letters += input("New Black Listed Letters (SEPERATED BY ','): ").split(",")
-        wg.wl_letters += input("New White Listed Letters (SEPERATED BY ','): ").split(",")
+        
+        
+        while True:
+            if input("Any WhiteListed letters? (y/n): ") == "n": break
+            data = [input("Letter: "), int(input("Position: "))-1]
+            wg.known_not_positions.append(data)
+            wg.wl_letters.append(data[0])
+
 
         while True:
             if input("Are there any known positions? (y/n): ") == "n": break
